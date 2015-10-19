2bf909ad55568ab5edb3b70dac42208e7513f350
Sebastian Fröhlich <froehlis@in.tum.de>
2015-10-19 16:15:28 +0200

    init

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  */
@@ -13,6 +17,21 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		
+		String name = "I";
+		try {
+			System.out.print("Please enter your name: ");
+	    	BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+			name = bufferRead.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		HelloUser hu = new HelloUser(name);
+		
+		hu.greetUser();
+	
 	}
 
 }
